fe97b17b23665116e73a21f02a61924a2341b1b6
Your Name <you@example.com]>
2026-01-30 13:38:29 +0000

    changed the server js file a bit

--- server.js
+++ server.js
@@ -1,20 +1,35 @@
+import express from 'express'; // Add this
+import cors from 'cors';       // Add this
 import 'dotenv/config';
 import sequelize from './config/database.js';
 
-async function connectToDatabase() {
+const app = express();
+if (!process.env.PORT) {
+  console.error("Set the PORT in .env")
+  process.exit(1);
+}
+const PORT = process.env.PORT; // Define your port
+
+app.use(cors());
+app.use(express.json());
+
+async function startServer() {
   try {
     await sequelize.authenticate();
     console.log('Connected to the database successfully!');
+
+    // Start listening for requests
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    app.get('/api', (req, res) => {
+      res.send({ status: 'Backend is reachable!' });
+    });
   } catch (err) {
     console.error('Database connection error:', err);
     process.exit(1);
   }
-}
-
-async function startServer() {
-  await connectToDatabase();
-  console.log('Server is running...');
-  process.stdin.resume();
 }
 
 startServer();
